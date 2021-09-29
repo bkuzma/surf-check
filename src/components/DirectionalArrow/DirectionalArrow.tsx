580d5ba939250d8d052a4b694fee59bbd6b7d0ec
Brian Kuzma <hello@briankuzma.com>
2021-09-29 08:37:17 +0200

    Update DirectionalArrow to accept size props

--- src/components/DirectionalArrow/DirectionalArrow.tsx
+++ src/components/DirectionalArrow/DirectionalArrow.tsx
@@ -4,16 +4,18 @@ import { ReactComponent as ArrowDown } from "../../assets/svg/arrow-down.svg"
 
 interface ArrowProps {
   degrees: number
+  height?: number
+  width?: number
 }
 
-function Arrow(props: ArrowProps) {
+const Arrow = ({ degrees, height = 20, width = 20 }: ArrowProps) => {
   return (
     <ArrowDown
       style={{
-        transform: `rotate(${props.degrees}deg)`,
+        transform: `rotate(${degrees}deg)`,
       }}
-      height="20"
-      width="20"
+      height={height}
+      width={width}
     />
   )
 }
