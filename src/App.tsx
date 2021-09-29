e65fe03e1224dae86dcf64dc992b8a3a7f5e7667
Brian Kuzma <hello@briankuzma.com>
2021-09-29 07:56:37 +0200

    Add dark mode color to description in header

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
             <h1 className="text-xl text-center font-medium text-gray-900 dark:text-yellow-300 uppercase tracking-wider">
               🚜 Jæren Surf Check 🏄
             </h1>
-            <p className="mt-2 text-sm text-center">
+            <p className="mt-2 text-sm text-center text-gray-900 dark:text-yellow-300">
               Bore Beach surf forecast with swell data from Magic Seaweed and
               wind data from YR.
             </p>
