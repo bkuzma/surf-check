7f1d35801d5a224e324c26adbab417ddb6f3dca4
Brian Kuzma <hello@briankuzma.com>
2021-10-13 09:07:51 +0200

    Enable JIT mode in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 module.exports = {
+  mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx,svg}", "./pages/**/*.{js,jsx,ts,tsx,svg}"],
   darkMode: "class",
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["even"],
-    },
   },
   plugins: [],
 }
