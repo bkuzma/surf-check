232e7bef05b0cb3c0fafe08800a3ea3b4c03972f
Brian Kuzma <hello@briankuzma.com>
2021-10-15 09:00:29 +0200

    Update Header to support dark mode
    Adds new active styles for nav links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,20 +17,24 @@ export default function Header() {
   const router = useRouter()
 
   return (
-    <header className="p-5 bg-green-500 dark:bg-green-900">
+    <header className="p-5 bg-green-500 dark:bg-green-900 text-gray-900 dark:text-yellow-300">
       <div className="flex justify-between md:justify-start md:space-x-12 items-center">
-        <h1 className="text-md md:text-xl text-center font-medium text-gray-900 dark:text-yellow-300 uppercase tracking-wider">
+        <h1 className="text-md md:text-xl text-center font-medium uppercase tracking-wider">
           🚜 Surf Check 🏄
         </h1>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-1">
             {LINKS.map((link) => (
               <li key={link.href}>
                 <Link href={link.href}>
                   <a
-                    className={classNames({
-                      underline: link.href === router.pathname,
-                    })}
+                    className={classNames(
+                      {
+                        "bg-green-600 dark:bg-green-800":
+                          link.href === router.pathname,
+                      },
+                      "uppercase text-xs tracking-wider font-medium p-2 hover:bg-green-600 dark:hover:bg-green-700 rounded"
+                    )}
                   >
                     {link.text}
                   </a>
@@ -40,7 +44,7 @@ export default function Header() {
           </ul>
         </nav>
       </div>
-      <p className="mt-2 text-xs text-gray-900 dark:text-yellow-300">
+      <p className="mt-4 text-xs">
         Bore Beach surf forecast with swell data from Magic Seaweed and wind
         data from YR.
       </p>
