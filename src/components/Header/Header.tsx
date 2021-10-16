7132678bb22a24bc3f74e483fcd59b4b9687b9ad
Brian Kuzma <hello@briankuzma.com>
2021-10-16 16:51:49 +0200

    Add Github link to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import classNames from "classnames"
 import Link from "next/link"
 import { useRouter } from "next/router"
 
+import GithubLogo from "../../assets/svg/github-logo.svg"
+
 const LINKS = [
   {
     href: "/",
@@ -20,11 +22,11 @@ export default function Header() {
     <header className="bg-green-500 dark:bg-green-900 text-gray-900 dark:text-yellow-300">
       <div className="container mx-auto p-5">
         <div className="flex justify-between md:justify-start md:space-x-12 items-center">
-          <h1 className="text-md md:text-xl text-center font-medium uppercase tracking-wider">
-            🚜 Surf Check 🏄
+          <h1 className="italic text-xl text-center font-medium uppercase tracking-wider">
+            Surf Check
           </h1>
           <nav>
-            <ul className="flex space-x-1">
+            <ul className="flex items-center space-x-1">
               {LINKS.map((link) => (
                 <li key={link.href}>
                   <Link href={link.href}>
@@ -34,7 +36,7 @@ export default function Header() {
                           "bg-green-600 dark:bg-green-800":
                             link.href === router.pathname,
                         },
-                        "uppercase text-xs tracking-wider font-medium p-2 hover:bg-green-600 dark:hover:bg-green-700 rounded"
+                        "block uppercase text-xs tracking-wider font-medium p-2 hover:bg-green-600 dark:hover:bg-green-700 rounded"
                       )}
                     >
                       {link.text}
@@ -42,6 +44,16 @@ export default function Header() {
                   </Link>
                 </li>
               ))}
+              <li className="">
+                <a
+                  className="rounded p-2 block"
+                  href="https://github.com/bkuzma/surf-check"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <GithubLogo height="20" width="20" />
+                </a>
+              </li>
             </ul>
           </nav>
         </div>
